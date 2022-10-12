f099e6e292d977c4803f35260c404089217a4b00
Nico Mollet <nico.mollet@gmail.com>
2022-10-12 17:20:25 +0200

    Update Zip.php

--- src/archive/Zip.php
+++ src/archive/Zip.php
@@ -1,15 +1,5 @@
 <?php
-
-/**
- * @copyright Copyright (c) 2020 Alonso Mora
- * @license   https://github.com/amoracr/yii2-backup/blob/master/LICENSE.md
- * @link      https://github.com/amoracr/yii2-backup#readme
- * @author    Alonso Mora <alonso.mora@gmail.com>
- */
-
-namespace amoracr\backup\archive;
-
-use amoracr\backup\archive\Archive;
+use nicomollet\backup\archive\Archive;
 use Yii;
 use yii\base\InvalidConfigException;
 use yii\helpers\FileHelper;
